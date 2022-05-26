219a6378584471a66c466563ba613c2ad9f11029
Charles Stevens <cs-interface@protonmail.ch>
2022-05-25 19:18:14 -0600

    I'm done

--- server.js
+++ server.js
@@ -16,16 +16,34 @@ server.use(express.static("whateverIwant"))
 server.listen(3000)
 
 server.get('/read', async (req, res) => {
-  const {rows} = await db.query(`select * from item`)
+  const { rows } = await db.query(`SELECT * FROM item`)
   res.json(rows)
 })
-
+server.get('/read/:id', async (req, res) => {
+  try {
+    const { rows } = await db.query(`SELECT * FROM item WHERE id = ${req.params.id}`)
+    res.json(rows[0])
+  } catch (err) { console.error(err.message) }
+})
 server.post('/create', async (req, res) => {
   const { title, body } = req.body
-  console.log(`Data is literally (${title}) (${body})`)
   try {
     await db.query(`INSERT INTO item (title, body) VALUES ('${title}', '${body}')`)
   } catch (err) { console.error(err.message) }
   res.send("You've hit the post")
 })
-
+server.patch('/update/:id', async (req, res) => {
+  const { id, title, body } = req.body
+  try {
+    await db.query(`
+      UPDATE item SET
+        title = '${title}',
+        body = '${body}'
+      WHERE id = ${id}`)
+  } catch (err) { console.error(err.message) }
+})
+server.delete('/delete/:id', async (req, res) => {
+  try {
+    await db.query(`DELETE FROM item WHERE id = ${req.params.id}`)
+  } catch (err) { console.error(err.message) }
+})
--- whateverIwant/index.js
+++ whateverIwant/index.js
@@ -21,12 +21,51 @@ async function updateDisplay() {
   const response = await request.json()
   response.forEach(entry => {
     const {id, title, body} = entry
-    $('#allTheTodoItems').append(`
-      <div class="todoItem">
-        <h3>${title}</h3>
-        <p>${body}</p>
-        <id hidden>${id}</id>
-      </div>
-    `)
+    const newItem = $(`
+        <div class="todoItem">
+          <h3>${title}</h3>
+          <div>
+            <p>${body}</p>
+            <img src="pencil.svg" id="${id}">
+          </div>
+        </div>
+      `)
+    $('#allTheTodoItems').append(newItem)
+    $('#allTheTodoItems > .todoItem:last-child > div > img').on('click', async e => {
+      $('#allTheTodoItems').html('')
+      const _request = await fetch(`http://localhost:3000/read/${e.target.id}`)
+      const {id, title, body} = JSON.parse(await _request.text())
+      $('#allTheTodoItems').append(`
+        <div class="todoItem">
+          <input type="text" name="title" id="title-update" value="${title}"/>
+          <input type="text" name="body" id="body-update" value="${body}"/>
+          <button type="submit" value="idk" id="update-submit-button">Submit</button>
+          <button type="submit" value="" id="delete">Delete</button>
+        </div>
+      `)
+      // made an update forum, now to actually perform the update
+      $('#update-submit-button').on('click', async () => {
+        const title = $('#title-update').val()
+        const body = $('#body-update').val()
+        $.ajax({
+          type: 'PATCH',
+          url: `http://localhost:3000/update/${id}`,
+          data: {
+            id: id,
+            title: title,
+            body: body
+          }
+        })
+        updateDisplay()
+      })
+      $('#delete').on('click', async () => {
+        $.ajax({
+          type: 'DELETE',
+          url: `http://localhost:3000/delete/${id}`
+        })
+        updateDisplay()
+      })
+    })
   })
 }
+
